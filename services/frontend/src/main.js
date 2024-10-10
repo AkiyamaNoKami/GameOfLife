c842b17e486eb99629c8ea91a1becd67f79bf5ac
Artem <ignatev@perco.ru>
2024-10-10 16:38:36 +0300

    Update routing between frontend and backend

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import axios from "axios";
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:5000';
+
+app.use(router);
+app.mount('#app');
