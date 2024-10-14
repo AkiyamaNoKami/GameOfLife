1dc2f09300c4cd677c2be52753415fbcb4e32c82
Artem <ignatev@perco.ru>
2024-10-14 16:59:49 +0300

    Edit main page

--- services/frontend/src/components/HelloWorld.vue
+++ services/frontend/src/components/HelloWorld.vue
@@ -1,6 +1,34 @@
 <template>
   <v-container>
+    <v-navigation-drawer permanent>
+      <v-toolbar>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title class="title">
+              Workspace
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-toolbar>
 
+      <v-divider></v-divider>
+
+      <v-list dense class="pt-0">
+        <v-list-item
+        v-for="item in items"
+        :key="item.title"
+        @click="handleClick(item)"
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </v-container>
 </template>
 
@@ -10,52 +38,16 @@ export default {
   name: 'HelloWorld',
 
   data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader/tree/next',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify/tree/next',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Roadmap',
-        href: 'https://vuetifyjs.com/introduction/roadmap/',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
+      items: [
+        { title: 'Home', icon: 'dashboard'},
+        { title: 'About', icon: 'question_answer'}
+      ],
+      right: null
   }),
+  methods: {
+    handleClick(item) {
+      console.log('Clicked od:', item.title);
+    },
+  },
 }
 </script>
--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,44 +1,11 @@
-// import { createApp } from 'vue'
-// import App from './App.vue'
-// import router from './router'
-// import vuetify from './plugins/vuetify'
-// import { loadFonts } from './plugins/webfontloader'
-//
-// loadFonts()
-//
-// createApp(App)
-//   .use(router)
-//   .use(vuetify)
-//   .mount('#app')
-
-
-// import '@mdi/font/css/materialdesignicons.css'; // Для Material Design Icons
-// import 'vuetify/styles'; // Стили для Vuetify
-// import { createVuetify } from 'vuetify'; // Правильный импорт Vuetify
-//
-// const vuetify = createVuetify({
-//     icons: {
-//         iconfont: 'mdi', // Указываем использование Material Design Icons
-//     },
-// });
-//
-// export default vuetify;
-
 
 import { createApp } from 'vue';
 import App from './App.vue';
-import vuetify from './plugins/vuetify'; // Убедитесь, что путь верный
-// import { createRouter, createWebHistory } from 'vue-router';
-// import routes from './router';
-import router from "./router"; // Проверьте правильный путь к вашему маршрутизатору
+import vuetify from './plugins/vuetify';
+import router from "./router";
 
 const app = createApp(App);
 
 app.use(vuetify);
 app.use(router);
-// app.use(createRouter({
-//     history: createWebHistory(),
-//     routes,
-// }));
-
 app.mount('#app');
